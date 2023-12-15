0fcc64ea56e12716d0b1982ddd98deae4c8dc5de
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2023-12-15 09:23:25 +0100

    feat(app): add icon to tabs

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import { Pokemons } from './src/Pages/Pokemons';
 import { Settings } from './src/Pages/Settings';
 
@@ -27,8 +28,28 @@ const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Pokemons" component={PokemonsScreen} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'ios-home' : 'ios-home';
+          } else if (route.name === 'Search') {
+            iconName = focused ? 'search' : 'search';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings-sharp' : 'settings-sharp';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#10b981',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      
+      <Tab.Screen name="Home" component={PokemonsScreen} />
       <Tab.Screen name="Search" component={SearchScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
